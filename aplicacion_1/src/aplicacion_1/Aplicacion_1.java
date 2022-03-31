724c354719d8aaa04c09cc3fbc8b069e2d52723e
unknown <SENA@ITAAPRREAFSP889.FormacionSena.RED>
2022-03-31 13:30:33 -0500

    Signed-off-by: unknown <SENA@ITAAPRREAFSP889.FormacionSena.RED>

---
+++ aplicacion_1/src/aplicacion_1/Aplicacion_1.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplicacion_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author SENA
+ */
+public class Aplicacion_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        System.out.println("texto");
+        Scanner entrada = new Scanner(System.in);
+        
+        
+        
+        
+        System.out.println("digite un numero1")  ;
+       
+        int a = entrada.nextInt();
+        
+        
+        
+         System.out.println("digite un numero2")  ;
+         int b = entrada.nextInt();
+         
+         //int c = a + b;
+         
+         System.out.println("la suma de los numeros es= " + (a+b))  ;
+         
+    }
+    
+}
+
